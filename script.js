e44330cf867a6af16d4b93086b6b22aac28c565d
Andrew Morrison <morrison.andrew422@gmail.com>
2020-10-07 23:38:34 -0400

    added score keeping, messages, and input fields among some other features.

--- script.js
+++ script.js
@@ -17,36 +17,76 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  // format playerSelction to ex. "Rock" not rock
+  // formats playerSelction to ex. "Rock" not rock
+  playerSelection = playerSelection.toLowerCase();
+  playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+
   if ((computerSelection === "Rock" && playerSelection === "Rock") ||
     (computerSelection === "Paper" && playerSelection === "Paper") ||
     (computerSelection === "Scissors" && playerSelection === "Scissors")) {
     console.log("You chose: " + playerSelection + ".");
     console.log("I chose: " + computerSelection + ".");
     console.log("It's a tie! Try again.");
+    console.log("My Score: " + computerScore);
+    console.log(("Your Score: " + playerScore));
   } else if ((computerSelection === "Rock" && playerSelection === "Paper") ||
     (computerSelection === "Paper" && playerSelection === "Scissors") ||
     (computerSelection === "Scissors" && playerSelection === "Rock")) {
     console.log("You chose: " + playerSelection + ".");
     console.log("I chose: " + computerSelection + ".");
-    console.log("Congratulations, you beat me!");
+    console.log("It's only a flesh wound.");
+    playerScore ++;
+    console.log("My Score: " + computerScore);
+    console.log(("Your Score: " + playerScore));
   } else if ((computerSelection === "Rock" && playerSelection === "Scissors") ||
     (computerSelection === "Paper" && playerSelection === "Rock") ||
     (computerSelection === "Scissors" && playerSelection === "Paper")) {
     console.log("You chose: " + playerSelection + ".");
     console.log("I chose: " + computerSelection + ".");
-    console.log("Mwahaha. I won.");
+    console.log("You're losing your touch.");
+    computerScore ++;
+    console.log("My Score: " + computerScore);
+    console.log(("Your Score: " + playerScore));
   } else {
     console.log("That's not a valid move.");
+    console.log(playerSelection, computerSelection)
   }
 }
 
-  
+function game() {
+  for (gamesPlayed = 0; gamesPlayed < 5; gamesPlayed++) {
+  playerSelection = prompt("What hand will you play?");
+  playRound(playerSelection, computerSelection);
+    if (playerScore === 3) {
+      console.log("Oh no... You won. :(");
+    } else if (computerScore === 3) {
+      console.log("You insulent mortal! You thought you could beat me? At a game of chance? Not likley.")
+    } else {
+      console.log((4 - gamesPlayed) + " more games left.");
+    }
+  }
+  if (computerScore > playerScore) {
+    alert("I won. Mwahahahahaah.");
+  } else if (computerScore < playerScore) {
+    alert("Lose... To YOU?! This can't be possible...");
+  } else {
+    alert("I guess a tie is to be expected in a game of chance. Well played human.")
+  }
+  if (prompt("Do you want to play again?") === "Yes" || "yes" || "Sure" || "sure") {
+    playerScore = 0;
+    computerScore = 0;
+    game();
+  } else {
+    alert("Have a nice day.");
+  }
+}
+
+let playerScore = 0;
+let computerScore = 0;
 
 
 
-
-
-const playerSelection = "Rock";
+let playerSelection = "SCISSORS";
 const computerSelection = computerPlay();
-playRound(playerSelection, computerSelection);
+game();
+
