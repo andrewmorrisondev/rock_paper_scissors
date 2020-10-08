7727e8fee51bcf5958d7673daf8eb6a68dda6ce3
Andrew Morrison <morrison.andrew422@gmail.com>
2020-10-07 22:09:11 -0400

    created basic command line R,P,S. Edit the string found on line 50 to play.

--- script.js
+++ script.js
@@ -16,4 +16,37 @@ function computerPlay() {
   }
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  // format playerSelction to ex. "Rock" not rock
+  if ((computerSelection === "Rock" && playerSelection === "Rock") ||
+    (computerSelection === "Paper" && playerSelection === "Paper") ||
+    (computerSelection === "Scissors" && playerSelection === "Scissors")) {
+    console.log("You chose: " + playerSelection + ".");
+    console.log("I chose: " + computerSelection + ".");
+    console.log("It's a tie! Try again.");
+  } else if ((computerSelection === "Rock" && playerSelection === "Paper") ||
+    (computerSelection === "Paper" && playerSelection === "Scissors") ||
+    (computerSelection === "Scissors" && playerSelection === "Rock")) {
+    console.log("You chose: " + playerSelection + ".");
+    console.log("I chose: " + computerSelection + ".");
+    console.log("Congratulations, you beat me!");
+  } else if ((computerSelection === "Rock" && playerSelection === "Scissors") ||
+    (computerSelection === "Paper" && playerSelection === "Rock") ||
+    (computerSelection === "Scissors" && playerSelection === "Paper")) {
+    console.log("You chose: " + playerSelection + ".");
+    console.log("I chose: " + computerSelection + ".");
+    console.log("Mwahaha. I won.");
+  } else {
+    console.log("That's not a valid move.");
+  }
+}
+
+  
+
+
+
+
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+playRound(playerSelection, computerSelection);
