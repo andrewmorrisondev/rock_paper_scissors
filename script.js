174556395bc239ed181974c3446230a15ebe491d
Andrew Morrison <morrison.andrew422@gmail.com>
2020-10-29 18:18:39 -0400

    added UI and reworked logic to accomodate

--- script.js
+++ script.js
@@ -1,92 +1,143 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Computer Picks Move
+///////////////////////////////////////////////////////////////////////////////////
 function getRandomNumber() {
   Math.floor(Math.random() * 3);
   return Math.floor(Math.random() * 3);
 }
 
-function computerPlay() {
-  let randNumber = getRandomNumber();
+function selectComputer() {
+  var randNumber = getRandomNumber();
   if (randNumber === 0) {
-    return "Rock";
+    randNumber = "rock";
+    let computerSelection = 0
+    return {
+      first: computerSelection,
+      second: randNumber
+    };
   } else if (randNumber === 1) {
-    return "Paper";
-  } else  if (randNumber === 2) {
-    return "Scissors";
-  } else  if (randNumber !== 0 || 1 || 2) {
-    return "Error";
+    randNumber = "paper";
+    let computerSelection = 1
+    return {
+      first: computerSelection,
+      second: randNumber
+    };
+  } else if (randNumber === 2) {
+    randNumber = "scissors";
+    let computerSelection = 2
+    return {
+      first: computerSelection,
+      second: randNumber
+    };
   }
 }
-
-function playRound(playerSelection, computerSelection) {
-  // formats playerSelction to ex. "Rock" not rock
-  playerSelection = playerSelection.toLowerCase();
-  playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-
-  if ((computerSelection === "Rock" && playerSelection === "Rock") ||
-    (computerSelection === "Paper" && playerSelection === "Paper") ||
-    (computerSelection === "Scissors" && playerSelection === "Scissors")) {
-    console.log("You chose: " + playerSelection + ".");
-    console.log("I chose: " + computerSelection + ".");
-    console.log("It's a tie! Try again.");
-    console.log("My Score: " + computerScore);
-    console.log(("Your Score: " + playerScore));
-  } else if ((computerSelection === "Rock" && playerSelection === "Paper") ||
-    (computerSelection === "Paper" && playerSelection === "Scissors") ||
-    (computerSelection === "Scissors" && playerSelection === "Rock")) {
-    console.log("You chose: " + playerSelection + ".");
-    console.log("I chose: " + computerSelection + ".");
-    console.log("It's only a flesh wound.");
-    playerScore ++;
-    console.log("My Score: " + computerScore);
-    console.log(("Your Score: " + playerScore));
-  } else if ((computerSelection === "Rock" && playerSelection === "Scissors") ||
-    (computerSelection === "Paper" && playerSelection === "Rock") ||
-    (computerSelection === "Scissors" && playerSelection === "Paper")) {
-    console.log("You chose: " + playerSelection + ".");
-    console.log("I chose: " + computerSelection + ".");
-    console.log("You're losing your touch.");
-    computerScore ++;
-    console.log("My Score: " + computerScore);
-    console.log(("Your Score: " + playerScore));
-  } else {
-    console.log("That's not a valid move.");
-    console.log(playerSelection, computerSelection)
-  }
+///////////////////////////////////////////////////////////////////////////////////
+// Listens for button click, determines moves, starts round
+///////////////////////////////////////////////////////////////////////////////////
+const rock = document.getElementsByClassName("rock");
+rock[0].addEventListener("mousedown", selectRock);
+function selectRock(e) {
+  clearBoard();
+  editComputerMessage(">You played rock.");
+  let playerSelection = 0;
+  let computerSelection = selectComputer();
+  thinkingAnimation(addDot);
+  keepScore(computerSelection, playerSelection);
+  setTimeout(editSecondComputerMessage, 3000, ">The Computer picked " + computerSelection.second);
+  return playerSelection;
 }
 
-function game() {
-  for (gamesPlayed = 0; gamesPlayed < 5; gamesPlayed++) {
-  playerSelection = prompt("What hand will you play?");
-  playRound(playerSelection, computerSelection);
-    if (playerScore === 3) {
-      console.log("Oh no... You won. :(");
-    } else if (computerScore === 3) {
-      console.log("You insulent mortal! You thought you could beat me? At a game of chance? Not likley.")
-    } else {
-      console.log((4 - gamesPlayed) + " more games left.");
-    }
-  }
-  if (computerScore > playerScore) {
-    alert("I won. Mwahahahahaah.");
-  } else if (computerScore < playerScore) {
-    alert("Lose... To YOU?! This can't be possible...");
-  } else {
-    alert("I guess a tie is to be expected in a game of chance. Well played human.")
-  }
-  if (prompt("Do you want to play again?") === "Yes" || "yes" || "Sure" || "sure") {
-    playerScore = 0;
-    computerScore = 0;
-    game();
-  } else {
-    alert("Have a nice day.");
-  }
+const paper = document.getElementsByClassName("paper");
+paper[0].addEventListener("mousedown", selectPaper);
+function selectPaper(e) {
+  clearBoard();
+  editComputerMessage(">You played paper.")
+  let playerSelection = 1;
+  let computerSelection = selectComputer();
+  thinkingAnimation(addDot);
+  keepScore(computerSelection, playerSelection);
+  setTimeout(editSecondComputerMessage, 3000, ">The Computer picked " + computerSelection.second);
+  return playerSelection;
 }
 
-let playerScore = 0;
-let computerScore = 0;
+const scissors = document.getElementsByClassName("scissors");
+scissors[0].addEventListener("mousedown", selectScissors);
+function selectScissors(e) {
+  clearBoard();
+  editComputerMessage(">You played scissors.")
+  let playerSelection = 2;
+  let computerSelection = selectComputer();
+  thinkingAnimation(addDot);
+  keepScore(computerSelection, playerSelection);
+  setTimeout(editSecondComputerMessage, 3000, ">The Computer picked " + computerSelection.second);
+  return playerSelection;
+}
+///////////////////////////////////////////////////////////////////////////////////
+// Updates Screen
+///////////////////////////////////////////////////////////////////////////////////
+function editComputerMessage(message) {
+  const computerMessage = document.querySelector(".computer-message");
+  computerMessage.textContent = message;
+}
+function editSecondComputerMessage(secondMessage) {
+  const secondComputerMessage = document.querySelector(".second-computer-message");
+  secondComputerMessage.textContent = secondMessage;
+}
+///////////////////////////////////////////////////////////////////////////////////
+// Keeps Score
+///////////////////////////////////////////////////////////////////////////////////
+function keepScore(computerSelection, playerSelection) {
+  let playerScore = document.querySelector(".player-score");
+
+  let computerScore = document.querySelector(".computer-score");
 
 
+  if ((computerSelection.first === 0 && playerSelection === 0) ||
+    (computerSelection.first === 1 && playerSelection === 1) ||
+    (computerSelection.first === 2 && playerSelection === 2)) {
+      // add line
+  } else if ((computerSelection.first === 0 && playerSelection === 1) ||
+    (computerSelection.first === 1 && playerSelection === 2) ||
+    (computerSelection.first === 2 && playerSelection === 0)) {
+      setTimeout(changeScore, 3000, playerScore);
+  } else if ((computerSelection.first === 0 && playerSelection === 2) ||
+    (computerSelection.first === 1 && playerSelection === 0) ||
+    (computerSelection.first === 2 && playerSelection === 1)) {
+      setTimeout(changeScore, 3000, computerScore);
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////
+// Animation
+///////////////////////////////////////////////////////////////////////////////////
+function addDot() {
+  const computerMessage = document.querySelector(".computer-message");
+  computerMessage.textContent = computerMessage.textContent + ".";
+}
 
-let playerSelection = "SCISSORS";
-const computerSelection = computerPlay();
-game();
+function thinkingAnimation(func, computerSelection) {
+  for(i = 0; i <1; i++) {
+    setTimeout(func, 1000);
+  };
+  for (i = 0; i < 1; i++) {
+    setTimeout(func, 2000);
+  };
+}
+///////////////////////////////////////////////////////////////////////////////////
+// Clears Screen
+///////////////////////////////////////////////////////////////////////////////////
+function clearBoard() {
+  const computerMessage = document.querySelector(".computer-message");
+  computerMessage.textContent = "";
 
+  const secondComputerMessage = document.querySelector(".second-computer-message");
+  secondComputerMessage.textContent = "";
+}
+///////////////////////////////////////////////////////////////////////////////////
+// Adds Number to Scoreboard
+///////////////////////////////////////////////////////////////////////////////////
+function changeScore(x) {
+  let newX = parseInt(x.textContent, 10);
+  let newScore = newX + 1;
+  x.textContent = newScore;
+};
+//////////////////////////////////////////////////////////////////////, playerScore
